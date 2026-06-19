fa24335fe601e1bf526669780dc4cdde5d2279b3
ahmadhumaidi <39653563+ahmadhumaidi@users.noreply.github.com>
2026-06-20 00:48:19 +0700

    Add external lead event list page

---
+++ app/Filament/Resources/ExternalLeadEventResource/Pages/ListExternalLeadEvents.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\ExternalLeadEventResource\Pages;
+
+use App\Filament\Resources\ExternalLeadEventResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListExternalLeadEvents extends ListRecords
+{
+    protected static string $resource = ExternalLeadEventResource::class;
+}
