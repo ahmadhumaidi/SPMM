7ab164a98615aa1bdf15967f5b8b806f50f42a70
ahmadhumaidi <39653563+ahmadhumaidi@users.noreply.github.com>
2026-06-19 19:21:01 +0700

    Configure AI education news sources

--- config/spmm.php
+++ config/spmm.php
@@ -10,4 +10,28 @@ return [
         'provider' => env('WHATSAPP_PROVIDER', 'log'),
         'default_country_code' => env('WHATSAPP_DEFAULT_COUNTRY_CODE', '62'),
     ],
+
+    'ai_news' => [
+        'openai_api_key' => env('OPENAI_API_KEY'),
+        'openai_model' => env('OPENAI_MODEL', 'gpt-4.1-mini'),
+        'sources' => [
+            'Pendidikan Tinggi Indonesia' => 'https://news.google.com/rss/search?q=pendidikan+tinggi+Indonesia&hl=id&gl=ID&ceid=ID:id',
+            'Kampus dan Kuliah Karyawan' => 'https://news.google.com/rss/search?q=kampus+kuliah+karyawan+Indonesia&hl=id&gl=ID&ceid=ID:id',
+            'PDDIKTI dan Perguruan Tinggi' => 'https://news.google.com/rss/search?q=PDDIKTI+perguruan+tinggi&hl=id&gl=ID&ceid=ID:id',
+        ],
+        'trend_sources' => [
+            'Google Trends Indonesia' => 'https://trends.google.com/trending/rss?geo=ID',
+        ],
+        'trend_keywords' => [
+            'kuliah online',
+            'kuliah karyawan',
+            'kelas hybrid',
+            'RPL',
+            'beasiswa',
+            'karier',
+            'sertifikasi',
+            'kampus terakreditasi',
+            'PDDIKTI',
+        ],
+    ],
 ];
