3cdf8ecfb1d3135f880f3d5ebd8a1145fa934bfe
ahmadhumaidi <39653563+ahmadhumaidi@users.noreply.github.com>
2026-06-20 00:45:16 +0700

    Add Meta lead webhook routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
 use App\Http\Controllers\PaymentWebhookController;
+use App\Http\Controllers\MetaLeadWebhookController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/webhooks/payment/{provider}', PaymentWebhookController::class)
     ->middleware('throttle:payment-webhooks')
     ->name('webhooks.payment');
+
+Route::get('/webhooks/leads/meta', [MetaLeadWebhookController::class, 'verify'])
+    ->middleware('throttle:meta-leads')
+    ->name('webhooks.leads.meta.verify');
+
+Route::post('/webhooks/leads/meta', [MetaLeadWebhookController::class, 'store'])
+    ->middleware('throttle:meta-leads')
+    ->name('webhooks.leads.meta');
