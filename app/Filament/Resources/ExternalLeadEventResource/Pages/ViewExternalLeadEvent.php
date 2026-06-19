38dcd399570a6beb4c1c568105acb695c3d5194f
ahmadhumaidi <39653563+ahmadhumaidi@users.noreply.github.com>
2026-06-20 00:48:35 +0700

    Add external lead event view page

---
+++ app/Filament/Resources/ExternalLeadEventResource/Pages/ViewExternalLeadEvent.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\ExternalLeadEventResource\Pages;
+
+use App\Filament\Resources\ExternalLeadEventResource;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewExternalLeadEvent extends ViewRecord
+{
+    protected static string $resource = ExternalLeadEventResource::class;
+}
